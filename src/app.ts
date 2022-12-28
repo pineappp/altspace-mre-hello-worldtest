ed15a6d0ee40489c4e68d28bb51123ae927abcad
pineappp <81400786+pineappp@users.noreply.github.com>
2022-12-28 00:20:43 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@
  * Licensed under the MIT License.
  */
 
-import * as MRE from '@microsoft/mixed-reality-extension-sdk';
 
 /**
  * The main class of this app. All the logic goes here.
@@ -13,6 +12,12 @@ export default class HelloWorld {
 	private cube: MRE.Actor = null;
 	private assets: MRE.AssetContainer;
 
+import { ActionState, BehaviorType, Guid, User } from 'Guid';
+
+export interface ActionEvent {
+	targetId: Guid;
+
+}
 	constructor(private context: MRE.Context) {
 		this.context.onStarted(() => this.started());
 	}
@@ -108,11 +113,18 @@ export default class HelloWorld {
 
 		// Trigger the grow/shrink animations on hover.
 		buttonBehavior.onHover('enter', () => {
-			// use the convenience function "AnimateTo" instead of creating the animation data in advance
-			MRE.Animation.AnimateTo(this.context, this.cube, {
-				destination: { transform: { local: { scale: { x: 0.5, y: 0.5, z: 0.5 } } } },
-				duration: 0.3,
-				easing: MRE.AnimationEaseCurves.EaseOutSine
+			MRE.Actor.CreateFromLibrary(this.context, {
+			// using the data we loaded earlier
+			resourceId:"artifact:2153444484094886470",
+			// Also apply the following generic actor properties.
+			actor: {
+				name: 'Balloon Fall From Sky',
+				// Parent the glTF model to the text actor, so the transform is relative to the text
+				parentId: menu.id,
+				transform: {
+					local: {
+						position: { x: 0, y: -1, z: 0 },
+						scale: { x: 0.4, y: 0.4, z: 0.4 }
 			});
 		});
 		buttonBehavior.onHover('exit', () => {
